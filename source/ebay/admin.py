3248258fdf02fa9c703705554bd0ed339952687d
Dastan T <dastan.turatbekov@gmail.com>
2022-12-13 19:02:26 +0600

    HW-56 этап-3: Админка. почти

--- source/ebay/admin.py
+++ source/ebay/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from ebay.models import Product, Basket, Order
+from ebay.models import Product, Basket, Order, OrderProduct
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -16,12 +16,22 @@ class BasketAdmin(admin.ModelAdmin):
 
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user_name', 'created_at']
+    list_display = ['id', 'user_name', 'created_at', 'get_products']
     list_filter = ['created_at']
     search_fields = ['product', 'user_name']
     exclude = []
+    filter_vertical = ['product']
+
+    def get_products(self):
+        for order in Order.objects.all():
+            lst = []
+            for prod in OrderProduct.objects.filter(order_id=order.pk):
+                lst.append(prod.product)
+            print(lst)
+            return lst
 
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Basket, BasketAdmin)
 admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderProduct)
