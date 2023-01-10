85dbb8de1396caf5badd37c22bd0e112254c1da6
Dastan T <dastan.turatbekov@gmail.com>
2023-01-10 12:43:18 +0600

    HW-62 задание 1: Регистрация реализована

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-# from accounts.views import RegisterView, UserDetailView, AllUsersView, UserChangeView, UserPasswordChangeView
+from accounts.views import RegisterView
 
 
 app_name = 'accounts'
@@ -8,7 +8,7 @@ app_name = 'accounts'
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    # path('register/', RegisterView.as_view(), name='register'),
+    path('register/', RegisterView.as_view(), name='register'),
     # path('<int:pk>/', UserDetailView.as_view(), name='detail'),
     # path('change/', UserChangeView.as_view(), name='change'),
     # path('change_password/', UserPasswordChangeView.as_view(), name='password_change'),
